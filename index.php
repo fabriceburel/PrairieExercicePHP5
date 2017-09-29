876524d8bc235d34f65c9cb214162069abe15a78
Fabrice BUREL <burelfabrice35@gmail.com>
2017-09-29 15:02:48 +0200

    exécution de l'exercice 5

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+function mafonction($var1,$var2)
+{
+  $var3=$var1.$var2;
+  echo $var3. "<br />";
+}
+
+
+ mafonction('fabrice','burel');
+ mafonction('Patrice','mon');
+ mafonction('Edouard','ma');
+
+  ?>
