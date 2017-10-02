7e3fc6a029dfefa0abc8c41d7f5fd28c995f48db
Fabrice BUREL <burelfabrice35@gmail.com>
2017-10-02 11:10:10 +0200

    ajout de la structure de langage return

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-function mafonction($var1,$var2)
-{
-  $var3=$var1.$var2;
-  echo $var3. "<br />";
-}
- mafonction('fabrice','burel');
- mafonction('Patrice','mon');
- mafonction('Edouard','ma');
+  $prenom ='fabrice';
+  $nom = 'burel';
+
+  function mafonction($var1,$var2)
+  {
+    $var3=$var1.$var2;
+    return $var3;
+  }
+  echo mafonction($prenom,$nom);
 ?>
