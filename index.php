fca38b396a21110e167373af45893a004b80aa07
Fabrice BUREL <burelfabrice35@gmail.com>
2017-10-02 09:38:17 +0200

    indendation du programme

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ function mafonction($var1,$var2)
   $var3=$var1.$var2;
   echo $var3. "<br />";
 }
-
-
  mafonction('fabrice','burel');
  mafonction('Patrice','mon');
  mafonction('Edouard','ma');
-
-  ?>
+?>
